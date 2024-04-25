8ca470461871832a9cded262c91e767fcc1bfad0
yunlinIT <yunlinit@gmail.com>
2024-04-25 09:54:29 +0900

    DB.sql

--- DB.sql
+++ DB.sql
@@ -16,12 +16,12 @@ CREATE TABLE `article` (
 
 CREATE TABLE `travelSchedule` (
 	`id`	INT(10)	NOT NULL,
-	`regDate`	DateTime	NULL,
-	`updateDate`	DateTime	NULL,
+	`regDate`	DATETIME	NULL,
+	`updateDate`	DATETIME	NULL,
 	`name`	CHAR(100)	NULL,
 	`description`	TEXT	NULL,
-	`startDate`	DateTime	NULL,
-	`endDate`	DateTime	NULL,
+	`startDate`	DATETIME	NULL,
+	`endDate`	DATETIME	NULL,
 	`step`	INT(2)	NULL,
 	`regionId`	INT(10)	NOT NULL,
 	`memberId`	INT(10)	NOT NULL	COMMENT 'member의 id'
@@ -45,10 +45,10 @@ CREATE TABLE `member` (
 
 CREATE TABLE `genFile` (
 	`id`	INT(10)	NOT NULL,
-	`regDate`	DateTime	NULL,
-	`updateDate`	DateTime	NULL,
-	`delDate`	DateTime	NULL,
-	`delStatus`	TinyINT(1)	NULL,
+	`regDate`	DATETIME	NULL,
+	`updateDate`	DATETIME	NULL,
+	`delDate`	DATETIME	NULL,
+	`delStatus`	TINYINT(1)	NULL,
 	`relTypeCode`	CHAR(50)	NULL,
 	`relId`	INT(10)	NULL,
 	`originFileName`	VARCHAR(100)	NULL,
@@ -246,35 +246,6 @@ ALTER TABLE `reply` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`i
 
 ########### TEST DATA ############
 
-DROP DATABASE IF EXISTS `Spring_AM_01`;
-CREATE DATABASE `Spring_AM_01`;
-USE `Spring_AM_01`;
-
-# article 테이블 생성
-CREATE TABLE article(
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    title CHAR(100) NOT NULL,
-    `body` TEXT NOT NULL
-);
-
-# member 테이블 생성
-CREATE TABLE `member`(
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    loginId CHAR(20) NOT NULL,
-    loginPw CHAR(80) NOT NULL,
-    `authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한 레벨 (3=일반,7=관리자)',
-    `name` CHAR(20) NOT NULL,
-    nickname CHAR(20) NOT NULL,
-    cellphoneNum CHAR(20) NOT NULL,
-    email CHAR(50) NOT NULL,
-    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴 여부 (0=탈퇴 전, 1=탈퇴 후)',
-    delDate DATETIME COMMENT '탈퇴 날짜'
-);
-
 
 # article TD 생성
 INSERT INTO article
@@ -336,7 +307,6 @@ nickname = '회원2',
 cellphoneNum = '01056785678',
 email = 'abcdef@gmail.com';
 
-ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
 
 UPDATE article
 SET memberId = 2
@@ -346,17 +316,6 @@ UPDATE article
 SET memberId = 3
 WHERE id IN (3,4);
 
-
-# board 테이블 생성
-CREATE TABLE board(
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    `code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice(공지사항), free(자유), QnA(질의응답) ...',
-    `name` CHAR(20) NOT NULL UNIQUE COMMENT '게시판 이름',
-    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제 여부 (0=삭제 전, 1=삭제 후)',
-    delDate DATETIME COMMENT '삭제 날짜'
-);
 
 # board TD 생성
 INSERT INTO board
@@ -377,7 +336,6 @@ updateDate = NOW(),
 `code` = 'QnA',
 `name` = '질의응답';
 
-ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER `memberId`;
 
 UPDATE article
 SET boardId = 1
@@ -391,18 +349,6 @@ UPDATE article
 SET boardId = 3
 WHERE id = 4;
 
-ALTER TABLE article ADD COLUMN hitCount INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `body`;
-
-# reactionPoint 테이블 생성
-CREATE TABLE reactionPoint(
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    memberId INT(10) UNSIGNED NOT NULL,
-    relTypeCode CHAR(50) NOT NULL COMMENT '관련 데이터 타입 코드',
-    relId INT(10) NOT NULL COMMENT '관련 데이터 번호',
-    `point` INT(10) NOT NULL
-);
 
 # reactionPoint 테스트 데이터 생성
 # 1번 회원이 1번 글에 싫어요
@@ -467,16 +413,7 @@ ON A.id = RP_SUM.relId
 SET A.goodReactionPoint = RP_SUM.goodReactionPoint,
 A.badReactionPoint = RP_SUM.badReactionPoint;
 
-# reply 테이블 생성
-CREATE TABLE reply (
-    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    regDate DATETIME NOT NULL,
-    updateDate DATETIME NOT NULL,
-    memberId INT(10) UNSIGNED NOT NULL,
-    relTypeCode CHAR(50) NOT NULL COMMENT '관련 데이터 타입 코드',
-    relId INT(10) NOT NULL COMMENT '관련 데이터 번호',
-    `body`TEXT NOT NULL
-);
+
 
 # 2번 회원이 1번 글에 댓글 작성
 INSERT INTO reply
@@ -577,147 +514,8 @@ ON R.id = RP_SUM.relId
 SET R.goodReactionPoint = RP_SUM.goodReactionPoint,
 R.badReactionPoint = RP_SUM.badReactionPoint;
 
-###############################################
+
 
 SELECT * FROM article;
 
-SELECT * FROM `member`;
-
-SELECT * FROM `board`;
-
-SELECT * FROM reactionPoint;
-
-SELECT * FROM `reply`;
-
-
-
-SELECT goodReactionPoint
-FROM article 
-WHERE id = 1
-
-INSERT INTO article
-(
-    regDate, updateDate, memberId, boardId, title, `body`
-)
-SELECT NOW(),NOW(), FLOOR(RAND() * 2) + 2, FLOOR(RAND() * 3) + 1, CONCAT('제목_',RAND()), CONCAT('내용_',RAND())
-FROM article;
-
-SELECT IFNULL(SUM(RP.point),0)
-FROM reactionPoint AS RP
-WHERE RP.relTypeCode = 'article'
-AND RP.relId = 3
-AND RP.memberId = 1;
-
-
-UPDATE article 
-SET title = '제목5'
-WHERE id = 5;
-
-UPDATE article 
-SET title = '제목11'
-WHERE id = 6;
-
-UPDATE article 
-SET title = '제목45'
-WHERE id = 7;
-
-SELECT FLOOR(RAND() * 2) + 2
-
-SELECT FLOOR(RAND() * 3) + 1
-
-
-SHOW FULL COLUMNS FROM `member`;
-DESC `member`;
-
-
-
-SELECT LAST_INSERT_ID();
-
-SELECT *
-FROM article AS A
-WHERE 1
-
-	AND boardId = 1
-
-			AND A.title LIKE CONCAT('%','0000','%')
-			OR A.body LIKE CONCAT('%','0000','%')
-
-ORDER BY id DESC
-
-SELECT COUNT(*)
-FROM article AS A
-WHERE 1
-AND boardId = 1
-AND A.title LIKE CONCAT('%','0000','%')
-OR A.body LIKE CONCAT('%','0000','%')
-ORDER BY id DESC
-
-
-SELECT hitCount
-FROM article
-WHERE id = 374;
-
-SELECT A.*
-FROM article AS A
-WHERE A.id = 1
-
-SELECT A.*, M.nickname AS extra__writer
-FROM article AS A
-INNER JOIN `member` AS M
-ON A.memberId = M.id
-WHERE A.id = 1
-
-# LEFT JOIN
-SELECT A.*, M.nickname AS extra__writer, RP.point
-FROM article AS A
-INNER JOIN `member` AS M
-ON A.memberId = M.id
-LEFT JOIN reactionPoint AS RP
-ON A.id = RP.relId AND RP.relTypeCode = 'article'
-GROUP BY A.id
-ORDER BY A.id DESC;
-
-# 서브쿼리
-SELECT A.*,
-IFNULL(SUM(RP.point),0) AS extra__sumReactionPoint,
-IFNULL(SUM(IF(RP.point > 0, RP.point, 0)),0) AS extra__goodReactionPoint,
-IFNULL(SUM(IF(RP.point < 0, RP.point, 0)),0) AS extra__badReactionPoint
-FROM (
-    SELECT A.*, M.nickname AS extra__writer 
-    FROM article AS A
-    INNER JOIN `member` AS M
-    ON A.memberId = M.id
-    ) AS A
-LEFT JOIN reactionPoint AS RP
-ON A.id = RP.relId AND RP.relTypeCode = 'article'
-GROUP BY A.id
-ORDER BY A.id DESC;
-
-# 조인
-SELECT A.*, M.nickname AS extra__writer,
-IFNULL(SUM(RP.point),0) AS extra__sumReactionPoint,
-IFNULL(SUM(IF(RP.point > 0, RP.point, 0)),0) AS extra__goodReactionPoint,
-IFNULL(SUM(IF(RP.point < 0, RP.point, 0)),0) AS extra__badReactionPoint
-FROM article AS A
-INNER JOIN `member` AS M
-ON A.memberId = M.id
-LEFT JOIN reactionPoint AS RP
-ON A.id = RP.relId AND RP.relTypeCode = 'article'
-GROUP BY A.id
-ORDER BY A.id DESC;
-
-
-SELECT *, COUNT(*)
-FROM reactionPoint AS RP
-GROUP BY RP.relTypeCode,RP.relId
-
-SELECT IF(RP.point > 0, '큼','작음')
-FROM reactionPoint AS RP
-GROUP BY RP.relTypeCode,RP.relId
-
-# 각 게시물의 좋아요, 싫어요 갯수
-SELECT RP.relTypeCode, RP.relId,
-SUM(IF(RP.point > 0,RP.point,0)) AS goodReactionPoint,
-SUM(IF(RP.point < 0,RP.point * -1,0)) AS badReactionPoint
-FROM reactionPoint AS RP
-GROUP BY RP.relTypeCode,RP.relId
+SELECT * FROM board;
