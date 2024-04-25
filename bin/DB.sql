21b879e4b116ee3ce630f7ca4cb50a2b2f44252c
Lysepten <tlstlsrbrb11@gmail.com>
2024-04-26 00:14:29 +0900

    feat : 캘린더 UI 생성

--- bin/DB.sql
+++ bin/DB.sql
@@ -10,21 +10,21 @@ CREATE TABLE `article` (
 	`hitCount`	INT(10)	NULL,
 	`title`	CHAR(100)	NULL	COMMENT '제목',
 	`body`	TEXT	NULL	COMMENT '내용',
-	`memberId`	INT(10)	NOT NULL	COMMENT 'member의 id',
-	`boardId`	INT(10)	NOT NULL
+	`memberId`	INT(10)	UNSIGNED NOT NULL	COMMENT 'member의 id',
+	`boardId`	INT(10)	UNSIGNED NOT NULL
 );
 
 CREATE TABLE `travelSchedule` (
 	`id`	INT(10)	NOT NULL,
-	`regDate`	DateTime	NULL,
-	`updateDate`	DateTime	NULL,
+	`regDate`	DATETIME	NULL,
+	`updateDate`	DATETIME	NULL,
 	`name`	CHAR(100)	NULL,
 	`description`	TEXT	NULL,
-	`startDate`	DateTime	NULL,
-	`endDate`	DateTime	NULL,
+	`startDate`	DATETIME	NULL,
+	`endDate`	DATETIME	NULL,
 	`step`	INT(2)	NULL,
-	`regionId`	INT(10)	NOT NULL,
-	`memberId`	INT(10)	NOT NULL	COMMENT 'member의 id'
+	`regionId`	INT(10)	UNSIGNED NOT NULL,
+	`memberId`	INT(10)	UNSIGNED NOT NULL	COMMENT 'member의 id'
 );
 
 CREATE TABLE `member` (
@@ -45,10 +45,10 @@ CREATE TABLE `member` (
 
 CREATE TABLE `genFile` (
 	`id`	INT(10)	NOT NULL,
-	`regDate`	DateTime	NULL,
-	`updateDate`	DateTime	NULL,
-	`delDate`	DateTime	NULL,
-	`delStatus`	TinyINT(1)	NULL,
+	`regDate`	DATETIME	NULL,
+	`updateDate`	DATETIME	NULL,
+	`delDate`	DATETIME	NULL,
+	`delStatus`	TINYINT(1)	NULL,
 	`relTypeCode`	CHAR(50)	NULL,
 	`relId`	INT(10)	NULL,
 	`originFileName`	VARCHAR(100)	NULL,
@@ -63,15 +63,15 @@ CREATE TABLE `genFile` (
 );
 
 CREATE TABLE `regionInfoTips` (
-	`id`	INT(10)	NOT NULL	COMMENT '여행지 설명 및 꿀팁 세션 아이디',
+	`id`	INT(10)	COMMENT '여행지 설명 및 꿀팁 세션 아이디',
 	`information`	TEXT	NULL	COMMENT '여행지에 대한 설명문',
 	`Voltage`	CHAR(30)	NULL	COMMENT '전압',
 	`language`	CHAR(20)	NULL	COMMENT '사용언어',
 	`climate`	CHAR(30)	NULL	COMMENT '기후',
-	`timeDifference`	TIME	NULL	COMMENT '시차',
-	`rate`	DECIMAL(10, 2)	NULL,
-	`tips`	CHAR(6)	NULL,
-	`regionId`	INT(10)	NOT NULL
+	`timeDifference`	CHAR(100)	NULL	COMMENT '시차',
+	`rate`	CHAR(100)	NULL,
+	`tips`	CHAR(100)	NULL,
+	`regionId`	INT(10)	UNSIGNED NOT NULL
 );
 
 CREATE TABLE `anniversaryDates` (
@@ -80,7 +80,7 @@ CREATE TABLE `anniversaryDates` (
 	`updateDate`	DATETIME	NULL,
 	`name`	VARCHAR(30)	NULL,
 	`day`	DATETIME	NULL,
-	`countryId`	INT(10)	NOT NULL
+	`countryId`	INT(10)	UNSIGNED NOT NULL
 );
 
 CREATE TABLE `country` (
@@ -99,7 +99,7 @@ CREATE TABLE `weather` (
 	`maxTemp`	DECIMAL(5,2)	NULL,
 	`humidity`	INT(3)	NULL,
 	`icon`	CHAR(3)	NULL,
-	`scheduleId`	INT(10)	NOT NULL
+	`scheduleId`	INT(10)	UNSIGNED NOT NULL
 );
 
 CREATE TABLE `fashion` (
@@ -109,7 +109,8 @@ CREATE TABLE `fashion` (
 	`name`	VARCHAR(100)	NULL,
 	`brand`	VARCHAR(50)	NULL,
 	`imageUrl`	TEXT	NULL,
-	`scheduleId`	INT(10)	NOT NULL
+	`gender`	TINYINT(1)	NULL,
+	`scheduleId`	INT(10)	UNSIGNED NOT NULL
 );
 
 CREATE TABLE `shoppingList` (
@@ -119,7 +120,7 @@ CREATE TABLE `shoppingList` (
 	`name`	VARCHAR(30)	NULL,
 	`description`	TEXT	NULL,
 	`imageUrl`	TEXT	NULL,
-	`regionId`	INT(10)	NOT NULL
+	`regionId`	INT(10)	UNSIGNED NOT NULL
 );
 
 CREATE TABLE `region` (
@@ -128,7 +129,8 @@ CREATE TABLE `region` (
 	`updateDate`	DATETIME	NULL,
 	`name`	VARCHAR(30)	NULL,
 	`naverRegionCord`	CHAR(20)	NULL,
-	`countryId`	INT(10)	NOT NULL
+	`imageUrl`	TEXT	NULL,
+	`countryId`	INT(10)	UNSIGNED NOT NULL
 );
 
 CREATE TABLE `recommendRegion` (
@@ -162,7 +164,7 @@ CREATE TABLE `regionSymbolicWord` (
 	`regDate`	DATETIME	NULL,
 	`updateDate`	DATETIME	NULL,
 	`symbolicWord`	CHAR(30)	NULL,
-	`countryId`	INT(10)	NOT NULL
+	`countryId`	INT(10)	UNSIGNED NOT NULL
 );
 
 CREATE TABLE `tag` (
@@ -191,14 +193,14 @@ CREATE TABLE `reactionPoint` (
 	`relTypeCode`	CHAR(50)	NULL,
 	`relId`	INT(10)	NULL,
 	`point`	INT(10)	NULL,
-	`memberId`	INT(10)	NOT NULL	COMMENT 'member의 id'
+	`memberId`	INT(10)	UNSIGNED NOT NULL	COMMENT 'member의 id'
 );
 
 CREATE TABLE `spotifyMember` (
 	`id`	INT(20)	NOT NULL	COMMENT '스포티파이 소셜로그인 회원id',
 	`name`	CHAR(50)	NULL	COMMENT '스포티파이 소셜로그인 회원 이름',
 	`email`	CHAR(100)	NULL	COMMENT '스포티파이 소셜로그인 회원 email',
-	`sporifyUserId`	INT(20)	NULL	COMMENT '스포티파이 회원의 id'
+	`sporifyUserId`	INT(20)	UNSIGNED NULL	COMMENT '스포티파이 회원의 id'
 );
 
 CREATE TABLE `reply` (
@@ -208,7 +210,7 @@ CREATE TABLE `reply` (
 	`relTypeCode`	CHAR(50)	NULL,
 	`relId`	INT(10)	NULL,
 	`body`	TEXT	NULL,
-	`memberId`	INT(10)	NOT NULL	COMMENT 'member의 id'
+	`memberId`	INT(10)	UNSIGNED NOT NULL	COMMENT 'member의 id'
 );
 
 CREATE TABLE `recommendSpotReview` (
@@ -216,27 +218,412 @@ CREATE TABLE `recommendSpotReview` (
 	`grade`	INT(10)	NULL	COMMENT '평점',
 	`regDate`	DATETIME	NULL	COMMENT '작성날짜',
 	`body`	TEXT	NULL	COMMENT '리뷰 내용',
-	`spotId`	INT(10)	NOT NULL	COMMENT '추천 장소 id',
-	`memberId`	INT(10)	NOT NULL	COMMENT 'member의 id'
+	`spotId`	INT(10)	UNSIGNED NOT NULL	COMMENT '추천 장소 id',
+	`memberId`	INT(10)	UNSIGNED NOT NULL	COMMENT 'member의 id'
 );
 
-ALTER TABLE `article` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `travelSchedule` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `member` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `genFile` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `regionInfoTips` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `anniversaryDates` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `country` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `weather` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `fashion` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `shoppingList` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `region` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `recommendRegion` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `tabList` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `recommendSpot` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `regionSymbolicWord` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `tag` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `board` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `reactionPoint` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `spotifyMember` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
-ALTER TABLE `reply` MODIFY `id` INT NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+## 주키 추가
+ALTER TABLE `article` MODIFY `id`  INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `travelSchedule` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `member` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `genFile` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `regionInfoTips` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `anniversaryDates` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `country` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `weather` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `fashion` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `shoppingList` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `region` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `recommendRegion` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `tabList` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `recommendSpot` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `regionSymbolicWord` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `tag` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `board` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `reactionPoint` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `spotifyMember` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+ALTER TABLE `reply` MODIFY `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, ADD PRIMARY KEY (`id`);
+
+## 관계키 추가
+/*
+ALTER TABLE `article` ADD CONSTRAINT `FK_member_TO_article_1` FOREIGN KEY (
+	`memberId`
+)
+REFERENCES `member` (
+	`id`
+);
+
+ALTER TABLE `article` ADD CONSTRAINT `FK_board_TO_article_1` FOREIGN KEY (
+	`boardId`
+)
+REFERENCES `board` (
+	`id`
+);
+
+ALTER TABLE `travelSchedule` ADD CONSTRAINT `FK_region_TO_travelSchedule_1` FOREIGN KEY (
+	`regionId`
+)
+REFERENCES `region` (
+	`id`
+);
+
+ALTER TABLE `travelSchedule` ADD CONSTRAINT `FK_member_TO_travelSchedule_1` FOREIGN KEY (
+	`memberId`
+)
+REFERENCES `member` (
+	`id`
+);
+
+ALTER TABLE `regionInfoTips` ADD CONSTRAINT `FK_region_TO_regionInfoTips_1` FOREIGN KEY (
+	`regionId`
+)
+REFERENCES `region` (
+	`id`
+);
+
+ALTER TABLE `anniversaryDates` ADD CONSTRAINT `FK_country_TO_anniversaryDates_1` FOREIGN KEY (
+	`countryId`
+)
+REFERENCES `country` (
+	`id`
+);
+
+ALTER TABLE `weather` ADD CONSTRAINT `FK_travelSchedule_TO_weather_1` FOREIGN KEY (
+	`scheduleId`
+)
+REFERENCES `travelSchedule` (
+	`id`
+);
+
+ALTER TABLE `fashion` ADD CONSTRAINT `FK_travelSchedule_TO_fashion_1` FOREIGN KEY (
+	`scheduleId`
+)
+REFERENCES `travelSchedule` (
+	`id`
+);
+
+ALTER TABLE `shoppingList` ADD CONSTRAINT `FK_region_TO_shoppingList_1` FOREIGN KEY (
+	`regionId`
+)
+REFERENCES `region` (
+	`id`
+);
+
+ALTER TABLE `region` ADD CONSTRAINT `FK_country_TO_region_1` FOREIGN KEY (
+	`countryId`
+)
+REFERENCES `country` (
+	`id`
+);
+
+ALTER TABLE `regionSymbolicWord` ADD CONSTRAINT `FK_region_TO_regionSymbolicWord_1` FOREIGN KEY (
+	`countryId`
+)
+REFERENCES `region` (
+	`id`
+);
+
+ALTER TABLE `reactionPoint` ADD CONSTRAINT `FK_member_TO_reactionPoint_1` FOREIGN KEY (
+	`memberId`
+)
+REFERENCES `member` (
+	`id`
+);
+
+ALTER TABLE `reply` ADD CONSTRAINT `FK_member_TO_reply_1` FOREIGN KEY (
+	`memberId`
+)
+REFERENCES `member` (
+	`id`
+);
+
+ALTER TABLE `recommendSpotReview` ADD CONSTRAINT `FK_recommendSpot_TO_recommendSpotReview_1` FOREIGN KEY (
+	`spotId`
+)
+REFERENCES `recommendSpot` (
+	`id`
+);
+
+ALTER TABLE `recommendSpotReview` ADD CONSTRAINT `FK_member_TO_recommendSpotReview_1` FOREIGN KEY (
+	`memberId`
+)
+REFERENCES `member` (
+	`id`
+);
+*/
+
+########### TEST DATA ############
+
+
+# article TD 생성
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목1',
+`body` = '내용1';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목2',
+`body` = '내용2';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목3',
+`body` = '내용3';
+
+INSERT INTO article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = '제목4',
+`body` = '내용4';
+
+# member TD 생성
+# (관리자)
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = 'admin',
+`authLevel` = 7,
+`name` = '관리자',
+nickname = '관리자',
+cellphoneNum = '01012341234',
+email = 'abcd@gmail.com';
+
+# (일반)
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = '회원1',
+nickname = '회원1',
+cellphoneNum = '01043214321',
+email = 'abcde@gmail.com';
+
+# (일반)
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = '회원2',
+nickname = '회원2',
+cellphoneNum = '01056785678',
+email = 'abcdef@gmail.com';
+
+
+UPDATE article
+SET memberId = 2
+WHERE id IN (1,2);
+
+UPDATE article
+SET memberId = 3
+WHERE id IN (3,4);
+
+
+# board TD 생성
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'NOTICE',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'FREE',
+`name` = '자유';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'QnA',
+`name` = '질의응답';
+
+
+UPDATE article
+SET boardId = 1
+WHERE id IN (1,2);
+
+UPDATE article
+SET boardId = 2
+WHERE id = 3;
+
+UPDATE article
+SET boardId = 3
+WHERE id = 4;
+
+
+# reactionPoint 테스트 데이터 생성
+# 1번 회원이 1번 글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+relTypeCode = 'article',
+relId = 1,
+`point` = -1;
+
+# 1번 회원이 2번 글에 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+relTypeCode = 'article',
+relId = 2,
+`point` = 1;
+
+# 2번 회원이 1번 글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'article',
+relId = 1,
+`point` = -1;
+
+# 2번 회원이 2번 글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'article',
+relId = 2,
+`point` = -1;
+
+# 3번 회원이 1번 글에 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 3,
+relTypeCode = 'article',
+relId = 1,
+`point` = 1;
+
+# article 테이블에 좋아요 관련 컬럼 추가
+ALTER TABLE article ADD COLUMN goodReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE article ADD COLUMN badReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
+
+# update join -> 기존 게시물의 good,bad RP 값을 RP 테이블에서 가져온 데이터로 채운다
+UPDATE article AS A
+INNER JOIN (
+    SELECT RP.relTypeCode,RP.relId,
+    SUM(IF(RP.point > 0, RP.point, 0)) AS goodReactionPoint,
+    SUM(IF(RP.point < 0, RP.point * -1, 0)) AS badReactionPoint
+    FROM reactionPoint AS RP
+    GROUP BY RP.relTypeCode, RP.relId
+) AS RP_SUM
+ON A.id = RP_SUM.relId
+SET A.goodReactionPoint = RP_SUM.goodReactionPoint,
+A.badReactionPoint = RP_SUM.badReactionPoint;
+
+
+
+# 2번 회원이 1번 글에 댓글 작성
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'article',
+relId = 1,
+`body` = '댓글 1';
+
+# 2번 회원이 1번 글에 댓글 작성
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'article',
+relId = 1,
+`body` = '댓글 2';
+
+# 3번 회원이 1번 글에 댓글 작성
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 3,
+relTypeCode = 'article',
+relId = 1,
+`body` = '댓글 3';
+
+# 3번 회원이 1번 글에 댓글 작성
+INSERT INTO reply
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'article',
+relId = 2,
+`body` = '댓글 4';
+
+# reply 테이블에 좋아요 관련 컬럼 추가
+ALTER TABLE reply ADD COLUMN goodReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
+ALTER TABLE reply ADD COLUMN badReactionPoint INT(10) UNSIGNED NOT NULL DEFAULT 0;
+
+# reactionPoint 테스트 데이터 생성
+# 1번 회원이 1번 댓글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+relTypeCode = 'reply',
+relId = 1,
+`point` = -1;
+
+# 1번 회원이 2번 댓글에 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 1,
+relTypeCode = 'reply',
+relId = 2,
+`point` = 1;
+
+# 2번 회원이 1번 댓글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'reply',
+relId = 1,
+`point` = -1;
+
+# 2번 회원이 2번 댓글에 싫어요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 2,
+relTypeCode = 'reply',
+relId = 2,
+`point` = -1;
+
+# 3번 회원이 1번 댓글에 좋아요
+INSERT INTO reactionPoint
+SET regDate = NOW(),
+updateDate = NOW(),
+memberId = 3,
+relTypeCode = 'reply',
+relId = 1,
+`point` = 1;
+
+# update join -> 기존 게시물의 good,bad RP 값을 RP 테이블에서 가져온 데이터로 채운다
+UPDATE reply AS R
+INNER JOIN (
+    SELECT RP.relTypeCode,RP.relId,
+    SUM(IF(RP.point > 0, RP.point, 0)) AS goodReactionPoint,
+    SUM(IF(RP.point < 0, RP.point * -1, 0)) AS badReactionPoint
+    FROM reactionPoint AS RP
+    GROUP BY RP.relTypeCode, RP.relId
+) AS RP_SUM
+ON R.id = RP_SUM.relId
+SET R.goodReactionPoint = RP_SUM.goodReactionPoint,
+R.badReactionPoint = RP_SUM.badReactionPoint;
+
+
+
+SELECT * FROM article;
+
+SELECT * FROM board;
--- src/main/webapp/WEB-INF/jsp/usr/Schedule/travelSchedule.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/Schedule/travelSchedule.jsp
@@ -1,45 +1,294 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
-<c:set var="pageTitle" value="HONEYTIPS PAGE"></c:set>
+<c:set var="pageTitle" value="Schedule PAGE"></c:set>
 <%@ include file="../common/head.jspf"%>
 <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<!-- 제이쿼리  -->
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-<script>
-      $(function(){
-         $('#datepicker').datepicker();
-      })
-</script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.structure.min.css">
+
+<style>
+
+@charset "UTF-8";
+
+@import url("https://fonts.googleapis.com/css?family=Raleway:400,300,600");
+
+#calendar-details .check-in, #calendar-details .check-out, #calendar-details .arrow {
+  float: left;
+  white-space: nowrap;
+  width: 39.3333333333%;
+  margin-left: 4%;
+}
+#calendar-details h5 {
+  font-size: 1.8rem;
+  margin-bottom: 0;
+}
+#calendar-details .check-in {
+  text-align: right;
+  margin-left: 0;
+}
+#calendar-details .arrow {
+  width: 13.3333333333%;
+  margin-top: 0.5em;
+  font-size: 2em;
+  line-height: 1em;
+  text-align: center;
+}
+#calendar-details .arrow:before {
+  display: inline-block;
+  content: "→";
+}
+
+.ui-datepicker * {
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+          user-select: none;
+  outline: 0;
+}
+.ui-datepicker.ui-datepicker-multi {
+  width: 100% !important;
+  max-width: 24em;
+  margin: 0 auto;
+}
+.ui-datepicker.ui-datepicker-multi .ui-datepicker-group {
+  width: 100%;
+}
+.ui-datepicker.ui-datepicker-multi.ui-datepicker-multi-2 {
+  max-width: 50em;
+}
+.ui-datepicker.ui-datepicker-multi.ui-datepicker-multi-2 .ui-datepicker-group {
+  width: 45%;
+}
+.ui-datepicker.ui-datepicker-multi.ui-datepicker-multi-2 .ui-datepicker-group.ui-datepicker-group-first {
+  padding-right: 5%;
+}
+.ui-datepicker.ui-datepicker-multi.ui-datepicker-multi-2 .ui-datepicker-group.ui-datepicker-group-last {
+  padding-left: 5%;
+}
+.ui-datepicker.ui-datepicker-multi.ui-datepicker-multi-3 {
+  max-width: 70em;
+}
+.ui-datepicker.ui-datepicker-multi.ui-datepicker-multi-3 .ui-datepicker-group {
+  width: 30%;
+}
+.ui-datepicker.ui-datepicker-multi.ui-datepicker-multi-3 .ui-datepicker-group.ui-datepicker-group-middle {
+  padding: 0 3.33%;
+}
+.ui-datepicker td a, .ui-datepicker td span {
+  border: 1px solid #fff;
+  border-radius: 0.5em;
+  text-align: center;
+  width: 2em;
+}
+.ui-datepicker td a.ui-state-active, .ui-datepicker td span.ui-state-active {
+  background-color: #1EAEDB;
+  color: #fff;
+}
+.ui-datepicker td a.ui-state-hover, .ui-datepicker td span.ui-state-hover {
+  border-color: #1EAEDB;
+}
+.ui-datepicker td.ui-datepicker-reserved a, .ui-datepicker td.ui-datepicker-reserved span {
+  background-color: #1EAEDB;
+  color: #fff;
+}
+.ui-datepicker td.ui-datepicker-reserved.ui-datepicker-checkin a, .ui-datepicker td.ui-datepicker-reserved.ui-datepicker-checkin span, .ui-datepicker td.ui-datepicker-reserved.ui-datepicker-checkout a, .ui-datepicker td.ui-datepicker-reserved.ui-datepicker-checkout span {
+  background-color: #fff;
+  border-color: #1EAEDB;
+  color: #1EAEDB;
+}
+.ui-datepicker .ui-datepicker-today a:not(.ui-state-hover), .ui-datepicker .ui-datepicker-today span:not(.ui-state-hover) {
+  border-color: #11DFC7;
+  color: #11DFC7;
+}
+.ui-datepicker .ui-datepicker-prev,
+.ui-datepicker .ui-datepicker-next {
+  border: 1px solid #fff;
+  border-radius: 0.5em;
+  text-align: center;
+  text-indent: 0;
+}
+.ui-datepicker .ui-datepicker-prev:before, .ui-datepicker .ui-datepicker-prev:after,
+.ui-datepicker .ui-datepicker-next:before,
+.ui-datepicker .ui-datepicker-next:after {
+  display: inline-block;
+}
+.ui-datepicker .ui-datepicker-prev.ui-state-hover,
+.ui-datepicker .ui-datepicker-next.ui-state-hover {
+  border-color: #1EAEDB;
+  top: 2px;
+}
+.ui-datepicker .ui-datepicker-prev:before {
+  content: "←";
+}
+.ui-datepicker .ui-datepicker-prev, .ui-datepicker .ui-datepicker-prev.ui-state-hover {
+  left: 10px;
+}
+.ui-datepicker .ui-datepicker-next:after {
+  content: "→";
+}
+.ui-datepicker .ui-datepicker-next, .ui-datepicker .ui-datepicker-next.ui-state-hover {
+  right: 10px;
+}
+
+hide {
+  opacity: 0;
+}
+
+show {
+  opacity: 1;
+}
+
+.fade-in {
+  -webkit-animation-duration: 1s;
+          animation-duration: 1s;
+  -webkit-animation-name: fade-in;
+          animation-name: fade-in;
+}
+
+/* Larger than mobile */
+@media (min-width: 400px) {
+  .ui-datepicker {
+    min-width: 18em;
+  }
+}
+/* Larger than phablet (also point when grid becomes active) */
+/* Larger than tablet */
+@media (min-width: 750px) {
+  .ui-datepicker {
+    min-width: 34em;
+  }
+
+  .arrow {
+    margin-top: 0;
+    font-size: 3em;
+  }
+}
+/* Larger than desktop */
+@media (min-width: 1000px) {
+  .ui-datepicker {
+    min-width: 51em;
+  }
+}
+
+/* Larger than Desktop HD */
+
+@-webkit-keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
+
 
 <script>
-   $(function() {
-       //input을 datepicker로 선언
-       $("#datepicker").datepicker({
-           dateFormat: 'yy-mm-dd' //달력 날짜 형태
-           ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
-           ,showMonthAfterYear:true // 월- 년 순서가아닌 년도 - 월 순서
-           ,changeYear: true //option값 년 선택 가능
-           ,changeMonth: true //option값  월 선택 가능                
-           ,showOn: "both" //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시  
-           ,buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif" //버튼 이미지 경로
-           ,buttonImageOnly: true //버튼 이미지만 깔끔하게 보이게함
-           ,buttonText: "선택" //버튼 호버 텍스트              
-           ,yearSuffix: "년" //달력의 년도 부분 뒤 텍스트
-           ,monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 텍스트
-           ,monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 Tooltip
-           ,dayNamesMin: ['일','월','화','수','목','금','토'] //달력의 요일 텍스트
-           ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'] //달력의 요일 Tooltip
-           ,minDate: "-5Y" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
-           ,maxDate: "+5y" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)  
-       });                    
-       
-       //초기값을 오늘 날짜로 설정해줘야 합니다.
-       $('#datepicker').datepicker('setDate', 'today'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)            
-   });
+$(function() {
+	  'use strict';
+
+	  var checkIn, checkOut, numberOfMonths = [2, 3],
+	      $calendar = $('#calendar').datepicker({
+	        numberOfMonths: numberOfMonths,
+	        prevText: '',
+	        nextText: '',
+	        beforeShowDay: function(date) {
+	          date = moment(date);
+
+	          var now = moment(),
+	              show = date.isAfter(now),
+	              css = '';
+
+	          if (checkIn && checkOut 
+	              && date.isSameOrAfter(checkIn)
+	              && date.isSameOrBefore(checkOut)) {
+	            css = 'ui-datepicker-reserved';
+
+	            if (date.isSame(checkIn)) css += ' ui-datepicker-checkin';
+	            if (date.isSame(checkOut)) css += ' ui-datepicker-checkout';
+	          }
+
+	          return [show, css];
+	        },
+	        onSelect: function(value) {
+	          var date = moment($calendar.datepicker('getDate'));
+
+	          if (checkIn && !checkOut
+	              && date.isSameOrAfter(checkIn)) 
+	            checkOut = date;
+	          else {
+	            checkIn = date;
+	            checkOut = null;
+	          }
+	          
+	          $('#check-in-date').text(checkIn ? checkIn.format('DD/MM/YYYY') : 'Choose a date');
+	          $('#check-out-date').text(checkOut ? checkOut.format('DD/MM/YYYY') : 'Choose a date');
+	        },
+	        onChangeMonthYear: function() {
+	          $calendar.addClass('fade-in');
+	        }
+	      }).on('animationend webkitAnimationEnd', function() {
+	        $calendar.removeClass('fade-in');
+	      });
+	  
+	  function resize() {
+	    var element = $('.ui-datepicker').get(0),
+	        style = window.getComputedStyle(element).getPropertyValue('min-width'), 
+	        value;
+	    
+	    switch (style) {
+	      case '765px': value = [2, 3]; break;
+	      case '510px': value = [2, 2]; break;
+	      default: value = [2, 1]; break;
+	    }
+	    
+	    if (numberOfMonths !== value) {
+	      if (checkIn) $calendar.datepicker('setDate', checkIn.toDate());
+	      
+	      $calendar.datepicker('option', 'numberOfMonths', numberOfMonths = value);
+	    }
+	  }
+
+	  $(window).on('resize', resize);
+	  
+	  resize();
+	});
 </script>
 
-<div id="datepicker"></div>
+<div class="container">
+  <div id="calendar-details">
+    <div class="check-in">
+      <h5>Check-In</h5>
+      <h6 id="check-in-date">Choose a date</h6>
+    </div>
+    <div class="arrow"></div>
+    <div class="check-out">
+      <h5>Check-Out</h5>
+      <h6 id="check-out-date">Choose a date</h6>
+    </div>
+  </div>
+  <div id="calendar"></div>
+</div>
+
 
 <%@ include file="../common/foot.jspf"%>
