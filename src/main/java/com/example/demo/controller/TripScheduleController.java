58753def4c863e78a390aa740d4733531c18b243
yunlinIT <yunlinit@gmail.com>
2024-05-03 09:55:00 +0900

    feat : Travel을 Trip으로 통일

--- src/main/java/com/example/demo/controller/TripScheduleController.java
+++ src/main/java/com/example/demo/controller/TripScheduleController.java
@@ -5,30 +5,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.service.TravelScheduleService;
+import com.example.demo.service.TripScheduleService;
 import com.example.demo.vo.Rq;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-public class TravelScheduleController {
+public class TripScheduleController {
 
 	@Autowired
-	private TravelScheduleService travelScheduleService;
+	private TripScheduleService tripScheduleService;
 
-	@RequestMapping("/usr/schedule/travelSchedule")
+	@RequestMapping("/usr/schedule/tripSchedule")
 	public String showMyPlanList() {
 
-		return "/usr/schedule/travelSchedule";
+		return "/usr/schedule/tripSchedule";
 	}
 
 	@RequestMapping("/usr/schedule/ticketing")
 	// 파라미터로 도시 id 받아야함
-	public String travelSchedule(HttpServletRequest req, String title, String content, String checkInDate, String checkOutDate, @RequestParam(defaultValue = "1") int regionId) {
+	public String tripSchedule(HttpServletRequest req, String title, String content, String checkInDate, String checkOutDate, @RequestParam(defaultValue = "1") int regionId) {
 
 		Rq rq = (Rq) req.getAttribute("rq");
 
-		travelScheduleService.insertTravelSchedule(title, content, checkInDate,
+		tripScheduleService.insertTripSchedule(title, content, checkInDate,
 				checkOutDate, rq.getLoginedMemberId(), regionId);
 
 		return "/usr/schedule/ticketing";
--- src/main/java/com/example/demo/controller/TripStyleRecommendedController.java
+++ src/main/java/com/example/demo/controller/TripStyleRecommendedController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.service.TravelScheduleService;
+import com.example.demo.service.TripScheduleService;
 import com.example.demo.service.TripStyleRecommendedService;
 import com.example.demo.util.Ut;
 import com.example.demo.vo.Fashion;
 import com.example.demo.vo.Rq;
 import com.example.demo.vo.ShoppingList;
-import com.example.demo.vo.TravelSchedule;
+import com.example.demo.vo.TripSchedule;
 import com.example.demo.vo.Weather;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -30,7 +30,7 @@ public class TripStyleRecommendedController {
 	private Rq rq;
 
 	@Autowired
-	private TravelScheduleService travelScheduleService;
+	private TripScheduleService tripScheduleService;
 	
 	@Autowired
 	private TripStyleRecommendedService tripStyleRecommendedService;
@@ -38,23 +38,23 @@ public class TripStyleRecommendedController {
 	// 액션 메서드
 	
 	@GetMapping("/usr/styleRecommended/create")
-	public String styleRecommended(HttpServletRequest req, Model model, @RequestParam(defaultValue = "1") int travelScheduleId) {
+	public String styleRecommended(HttpServletRequest req, Model model, @RequestParam(defaultValue = "1") int tripScheduleId) {
 
 //		Rq rq = (Rq) req.getAttribute("rq");
 
-		TravelSchedule travelSchedule = travelScheduleService.getTravelScheduleById(travelScheduleId);
+		TripSchedule tripSchedule = tripScheduleService.getTripScheduleById(tripScheduleId);
 
-		if (travelSchedule == null) {
+		if (tripSchedule == null) {
 			return rq.historyBackOnView("존재하지 않는 일정 입니다.");
 		}
 		
-//		if(rq.getLoginedMemberId() != travelSchedule.getMemberId()) {
+//		if(rq.getLoginedMemberId() != tripSchedule.getMemberId()) {
 //			return rq.historyBackOnView("로그인한 사용자가 일정 작성자가 아닙니다.");
 //		}
 
 
-		model.addAttribute("travelSchedule", travelSchedule);
-		model.addAttribute("travelScheduleId", travelScheduleId);
+		model.addAttribute("tripSchedule", tripSchedule);
+		model.addAttribute("tripScheduleId", tripScheduleId);
 
 		return "usr/styleRecommended/create";
 	}
@@ -62,7 +62,7 @@ public class TripStyleRecommendedController {
 	
 	@PostMapping("/usr/styleRecommended/doCreate")
 	@ResponseBody
-	public String styleRecommendedDoCreate(String weatherDatas, String fashionDatas, String shoppingListDatas, Integer travelScheduleId) {
+	public String styleRecommendedDoCreate(String weatherDatas, String fashionDatas, String shoppingListDatas, Integer tripScheduleId) {
 		//todo 일정id가 일치하는지 확인 로직 추가 필요. (로그인기능 문제 없는거 확인 후 추가할 것)
 		
 		ObjectMapper mapper = new ObjectMapper();
@@ -70,7 +70,7 @@ public class TripStyleRecommendedController {
 		List<Fashion> fashions = null;
 		List<ShoppingList> shoppingLists = null;
 		
-		if(travelScheduleId == null) {
+		if(tripScheduleId == null) {
         	return rq.historyBackOnView("잘못된 접근입니다. 여행 일정 id가 없습니다.");
 		}
 		
@@ -78,7 +78,7 @@ public class TripStyleRecommendedController {
         try {
         	 weathers = mapper.readValue(weatherDatas, new TypeReference<List<Weather>>(){});
         	 for(Weather weather: weathers) {
-        		 weather.setScheduleId(travelScheduleId);
+        		 weather.setScheduleId(tripScheduleId);
         	 }
              
         } catch (Exception e) {
@@ -91,7 +91,7 @@ public class TripStyleRecommendedController {
         try {
         	fashions = mapper.readValue(fashionDatas, new TypeReference<List<Fashion>>(){});
 	       	 for(Fashion fashion: fashions) {
-	       		fashion.setScheduleId(travelScheduleId);
+	       		fashion.setScheduleId(tripScheduleId);
 	    	 }  
         } catch (Exception e) {
         	e.printStackTrace();
@@ -102,7 +102,7 @@ public class TripStyleRecommendedController {
         try {
         	shoppingLists = mapper.readValue(shoppingListDatas, new TypeReference<List<ShoppingList>>(){});
 	       	 for(ShoppingList shoppingItem: shoppingLists) {
-	       		shoppingItem.setScheduleId(travelScheduleId);
+	       		shoppingItem.setScheduleId(tripScheduleId);
 	    	 }  
         } catch (Exception e) {
         	e.printStackTrace();
