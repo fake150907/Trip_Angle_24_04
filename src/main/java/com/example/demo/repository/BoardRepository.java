a86bf4fc52b04700abf51fc82390cf7014657f55
lysepten <tlstlsrbrb11@gmail.com>
2024-05-17 11:13:39 +0900

    docs : ArticleRepository, BoardRepository, FashionRepository 주석처리 작업

--- src/main/java/com/example/demo/repository/BoardRepository.java
+++ src/main/java/com/example/demo/repository/BoardRepository.java
@@ -8,12 +8,13 @@ import com.example.demo.vo.Board;
 @Mapper
 public interface BoardRepository {
 
-	@Select("""
-				SELECT *
-				FROM board
-				WHERE id = #{boardId}
-				AND delStatus = 0;
-			""")
-	public Board getBoardById(int boardId);
+    // boardId에 해당하는 게시글을 모두 조회하는 메서드
+    @Select("""
+            SELECT *
+            FROM board
+            WHERE id = #{boardId}
+            AND delStatus = 0;
+            """)
+    public Board getBoardById(int boardId);
 
 }
--- src/main/java/com/example/demo/repository/FashionRepository.java
+++ src/main/java/com/example/demo/repository/FashionRepository.java
@@ -2,32 +2,30 @@ package com.example.demo.repository;
 
 import java.util.List;
 
-
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.example.demo.vo.Fashion;
-import com.example.demo.vo.Weather;
-
 
 @Mapper
 public interface FashionRepository {
 
-	@Insert({
-	    "<script>",
-	    "INSERT INTO Fashion (regDate, updateDate, `name`, brand, description, imageUrl, gender, scheduleId) VALUES ",
-	    "<foreach collection='fashions' item='fashion' separator=','>",
-	    "(NOW(), NOW(), #{fashion.name}, #{fashion.brand}, #{fashion.description}, #{fashion.imageUrl}, #{fashion.gender}, #{fashion.scheduleId})",
-	    "</foreach>",
-	    "</script>"
-	})
-	public void writeFashionList(List<Fashion> fashions);
+    // 패션 리스트를 일괄 등록하는 메서드
+    @Insert({
+        "<script>",
+        "INSERT INTO Fashion (regDate, updateDate, `name`, brand, description, imageUrl, gender, scheduleId) VALUES ",
+        "<foreach collection='fashions' item='fashion' separator=','>",
+        "(NOW(), NOW(), #{fashion.name}, #{fashion.brand}, #{fashion.description}, #{fashion.imageUrl}, #{fashion.gender}, #{fashion.scheduleId})",
+        "</foreach>",
+        "</script>"
+    })
+    public void writeFashionList(List<Fashion> fashions);
 
-
-	@Select(
-			"""
-			SELECT * FROM Fashion WHERE scheduleId = ${scheduleId}	
-			""")
-	public List<Fashion> getFashionsFromScheduleId(int scheduleId);
+    // 특정 일정(scheduleId)에 속하는 패션 리스트를 조회하는 메서드
+    @Select(
+            """
+            SELECT * FROM Fashion WHERE scheduleId = ${scheduleId}    
+            """)
+    public List<Fashion> getFashionsFromScheduleId(int scheduleId);
 }
