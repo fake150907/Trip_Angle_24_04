ab18e18ac15939542183f154a4b6ae37a9940e02
fake150907 <yungayeon223@gmail.com>
2024-05-17 10:49:58 +0900

    docs : memberController, interceptor 메세지 수정, PlaceInfo관련, Ut 주석달기

--- src/main/java/com/example/demo/crawling/PlaceInfoDto.java
+++ src/main/java/com/example/demo/crawling/PlaceInfoDto.java
@@ -8,28 +8,43 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlaceInfoDto {
+	// 추천장소 id
 	private int id;
+	// 추천장소 정보 생성날짜
 	private String regDate;
+	// 추천장소 정보 수정날짜
 	private String updateDate;
+	// 추천장소 이름
 	private String name;
+	// 추천장소 주소
 	private String address;
+	// 추천장소 평점
 	private String grade;
+	// 추천장소 시설 정보
 	private String facilities;
 	// private String operatingTime;
+	// 추천장소 전화번호
 	private String phoneNumber;
+	// 추천장소 리뷰수
 	private String reviewCount;
+	// 추천장소 가격
 	private String price;
 
+	// 추천장소 사진 imageUrl
 	private String imageUrl1;
 	private String imageUrl2;
 	private String imageUrl3;
 	private String imageUrl4;
 	private String imageUrl5;
 
+	// 추천장소 네이버코드
 	private String naverSpotCord;
+	// 추천장소 카테고리 id
 	private int tabId;
+	// 추천장소 지역 id
 	private int regionId;
 
+	// 생성자
 	public PlaceInfoDto(String name, String address, String phoneNum, String facilities, String grade, String imgUrl1,
 			String imgUrl2, String imgUrl3, String imgUrl4, String imgUrl5, String reviewCount, String price) {
 		super();
@@ -47,6 +62,7 @@ public class PlaceInfoDto {
 		this.price = price;
 	}
 
+	// 생성자
 	public PlaceInfoDto(String naverSpotCord, int tabId, int regionId) {
 		super();
 		this.naverSpotCord = naverSpotCord;
