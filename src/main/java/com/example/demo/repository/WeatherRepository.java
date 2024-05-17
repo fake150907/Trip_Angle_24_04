c1c44ad2a1c737c0c5a237a99ed7cb7c8d113abb
lysepten <tlstlsrbrb11@gmail.com>
2024-05-17 11:25:36 +0900

    docs : 리포지터리 주석처리 작업 완료

--- src/main/java/com/example/demo/repository/WeatherRepository.java
+++ src/main/java/com/example/demo/repository/WeatherRepository.java
@@ -2,31 +2,30 @@ package com.example.demo.repository;
 
 import java.util.List;
 
-
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.example.demo.vo.Weather;
 
-
 @Mapper
 public interface WeatherRepository {
-	
-	@Insert({
-	    "<script>",
-	    "INSERT INTO Weather (regDate, updateDate, day, minTemp, maxTemp, humidity, icon, scheduleId) VALUES ",
-	    "<foreach collection='weathers' item='weather' separator=','>",
-	    "(NOW(), NOW(), #{weather.day}, #{weather.minTemp}, #{weather.maxTemp}, #{weather.humidity}, #{weather.icon}, #{weather.scheduleId})",
-	    "</foreach>",
-	    "</script>"
-	})
-	public void writeWeatherList(List<Weather> weathers);
+    
+    // 날씨 정보를 일괄 등록하는 메서드
+    @Insert({
+        "<script>",
+        "INSERT INTO Weather (regDate, updateDate, day, minTemp, maxTemp, humidity, icon, scheduleId) VALUES ",
+        "<foreach collection='weathers' item='weather' separator=','>",
+        "(NOW(), NOW(), #{weather.day}, #{weather.minTemp}, #{weather.maxTemp}, #{weather.humidity}, #{weather.icon}, #{weather.scheduleId})",
+        "</foreach>",
+        "</script>"
+    })
+    public void writeWeatherList(List<Weather> weathers);
 
-
-	@Select(
-			"""
-			SELECT * FROM Weather WHERE scheduleId = ${scheduleId}	
-			""")
-	public List<Weather> getWeathersFromScheduleId(int scheduleId);
+    // 특정 일정(scheduleId)에 속하는 날씨 정보를 조회하는 메서드
+    @Select(
+            """
+            SELECT * FROM Weather WHERE scheduleId = ${scheduleId}    
+            """)
+    public List<Weather> getWeathersFromScheduleId(int scheduleId);
 }
